91d63710bb3db4a7164d62e18ca9e546e856cea6
Herman Singh <kartstig@gmail.com>
2018-08-08 10:03:21 -0400

    cast() should only accept map

--- lib/repo/json_patch_type.ex
+++ lib/repo/json_patch_type.ex
@@ -1,8 +1,12 @@
 defmodule ExAudit.Type.JsonPatch do
   @behaviour Ecto.Type
 
-  def cast(a), do: {:ok, a}
+  def cast(a) when is_map(a) do: {:ok, a}
+  def cast(_), do: :error
+
   def dump(patch), do: {:ok, Jason.encode!(patch)}
+
   def load(json), do: {:ok, Jason.decode!(json)}
-  def type, do: :json
+
+  def type, do: :map
 end
